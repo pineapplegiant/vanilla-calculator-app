766f8dbd0ca4fac927faf697005da4f50b0bd24c
Guillermo Rodriguez <guillermor@mail.usf.edu>
2019-09-16 20:28:36 -0600

    First commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+  return a + b;
+}
+function sub(a, b) {
+  return a - b;
+}
+function mult(a, b) {
+  return a + b;
+}
+function div(a, b) {
+  return a / b;
+}
+
+const inputNum = document.querySelector(".input-num");
+document.
